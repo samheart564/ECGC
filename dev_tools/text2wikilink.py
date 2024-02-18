035b4ab2eb78ede4b0ee34feb835f9e4a04cb12e
samheart564 <samheart564@gmail.com>
2024-02-18 11:49:27 -0800

    fixed code

--- dev_tools/text2wikilink.py
+++ dev_tools/text2wikilink.py
@@ -1,30 +1,27 @@
 def generate_wiki_link(page_name):
-    # Construct the URL based on the page name
     base_url = "https://azurlane.koumakan.jp/wiki/"
     formatted_page_name = page_name.replace(" ", "_")
     full_url = base_url + formatted_page_name
-
-    # Generate the HTML link
-    link_html = f'<a rel="noopener noreferrer" target="_blank" href="{full_url}" title="{page_name}">{page_name}</a>'
-
+    link_html = f'<a rel="noopener noreferrer" target="_blank" href="{full_url}" title="{page_name}">{page_name}</a>\n'
     return link_html
 
 def append_to_html_file(file_name, link):
-    # Append the link to the existing HTML file
-    with open(file_name, "a") as file:
-        file.write(link)
+    with open(file_name, "a") as f:
+        f.write(link)
 
 def main():
-    # Get user input for the page name
-    page_name = input("Enter the page name: ")
+    exit_commands = {"q", "quit", "exit"}
 
-    # Generate the link
-    link = generate_wiki_link(page_name)
+    while True:
+        page_name = input("Enter the page name (or 'q', 'quit', 'exit' to exit): ")
+        stripped_page_name = page_name.strip().lower()
 
-    # Append the link to test1.html
-    append_to_html_file("test1.html", link)
-
-    print(f"Link appended to test1.html: {link}")
+        if stripped_page_name in exit_commands:
+            print("Exiting Loop")
+            break
+        else:
+            link = generate_wiki_link(page_name)
+            append_to_html_file("ECGC\\dev_tools\\test1.html", link)
 
 if __name__ == "__main__":
     main()
