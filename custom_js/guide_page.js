858a560a10037ac058894b77f28dc6a3c747a769
samheart564 <samheart564@gmail.com>
2024-10-20 14:37:17 -0700

    autoformat with prettier

--- custom_js/guide_page.js
+++ custom_js/guide_page.js
@@ -1,6 +1,5 @@
-
 //Table Scrolling
-const slider = document.querySelector('.table-responsive');
+const slider = document.querySelector(".table-responsive");
 let mouseDown = false;
 let startX, scrollLeft;
 
@@ -14,18 +13,20 @@ if (slider) {
     mouseDown = false;
   };
 
-  slider.addEventListener('mousemove', (e) => {
+  slider.addEventListener("mousemove", (e) => {
     e.preventDefault();
-    if(!mouseDown) { return; }
+    if (!mouseDown) {
+      return;
+    }
     const x = e.pageX - slider.offsetLeft;
     const scroll = x - startX;
     slider.scrollLeft = scrollLeft - scroll;
   });
 
   // Event listeners
-  slider.addEventListener('mousedown', startDragging, false);
-  slider.addEventListener('mouseup', stopDragging, false);
-  slider.addEventListener('mouseleave', stopDragging, false);
+  slider.addEventListener("mousedown", startDragging, false);
+  slider.addEventListener("mouseup", stopDragging, false);
+  slider.addEventListener("mouseleave", stopDragging, false);
 }
 
 //SideNav
@@ -39,17 +40,21 @@ function toggleFunction() {
     sidenavButton.classList.toggle("custom-sidenav-collapse");
     main.classList.toggle("custom-sidenav-collapse");
 
-    if (sidenav.classList.contains("toggle") && sidenav.classList.contains("custom-sidenav-collapse")) {
+    if (
+      sidenav.classList.contains("toggle") &&
+      sidenav.classList.contains("custom-sidenav-collapse")
+    ) {
       sidenav.classList.remove("toggle");
       sidenavButton.classList.remove("toggle");
     }
-  }
-  
-  else {
+  } else {
     sidenav.classList.toggle("toggle");
     sidenavButton.classList.toggle("toggle");
-    
-    if (sidenav.classList.contains("custom-sidenav-collapse") && sidenav.classList.contains("toggle")) {
+
+    if (
+      sidenav.classList.contains("custom-sidenav-collapse") &&
+      sidenav.classList.contains("toggle")
+    ) {
       sidenav.classList.remove("custom-sidenav-collapse");
       sidenavButton.classList.remove("custom-sidenav-collapse");
       main.classList.remove("custom-sidenav-collapse");
@@ -65,7 +70,7 @@ function smToggleFunction() {
 
 function catchupToggleFunction() {
   // var button = document.getElementById("catchupButton");
-  var content = document.getElementById("catchupToggleContent")
+  var content = document.getElementById("catchupToggleContent");
   var icon = document.getElementById("catchupToggle");
   var button = document.getElementById("catchupButton");
 
@@ -74,20 +79,15 @@ function catchupToggleFunction() {
     button.classList.toggle("active");
     icon.classList.remove("fa-angle-down");
     icon.classList.add("fa-angle-up");
-  }
-
-  else {
+  } else {
     content.classList.toggle("d-none");
     button.classList.toggle("active");
     icon.classList.remove("fa-angle-up");
     icon.classList.add("fa-angle-down");
   }
-
 }
-
 
 // document.getElementsByClassName("toc_h_one").onclick = toggleFunction();
 // document.getElementsByClassName("toc_h_two").onclick = toggleFunction();
 // document.getElementsByClassName("toc_h_three").onclick = toggleFunction();
 // document.getElementById("toc_h_four").addEventListener("onclick", toggleFunction())
-
